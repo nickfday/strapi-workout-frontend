1f3f6800a4202acaa2c79c4a9e20453579ec5804
Nicholas Finley-Day <nickfday@gmail.com>
2020-06-04 18:24:52 +0100

    static props example

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import App from 'next/app';
-import { ApolloProvider } from '@apollo/react-hooks';
+// import React from 'react';
+// import App from 'next/app';
+// import { ApolloProvider } from '@apollo/react-hooks';
 import Page from '../components/Page';
-// import { withApollo } from '../../lib/apollo';
+// // import { withApollo } from '../../lib/apollo';
 
-import withData from '../util/apollo-client';
+// import withData from '../util/apollo-client';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps, apollo } = this.props;
-    return (
-      <ApolloProvider client={apollo}>
-        <Page>
-          <Component {...pageProps} />
-        </Page>
-      </ApolloProvider>
-    );
-  }
+// class MyApp extends App {
+//   render() {
+//     const { Component, pageProps, apollo } = this.props;
+//     return (
+//       <ApolloProvider client={apollo}>
+//         <Page>
+//           <Component {...pageProps} />
+//         </Page>
+//       </ApolloProvider>
+//     );
+//   }
+// }
+
+// // Wraps all components in the tree with the data provider
+// export default withData(MyApp, { ssr: false });
+// // export default withApollo({ ssr: true })(MyApp);
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
 }
 
-// Wraps all components in the tree with the data provider
-export default withData(MyApp, { ssr: false });
-// export default withApollo({ ssr: true })(MyApp);
+export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ComponentBuilderPage from 'components/ComponentBuilder/page';
 import { ArticleTeaserPage } from 'components/Article/page';
 import { MaterialAdvancedGridList } from 'components/MaterialUI/Layout/GridList/AdvancedGridList';
+import { getExercises } from '../../lib/api';
 
-const Homepage = () => {
+const Homepage = (allExercises) => {
+  console.log(allExercises);
   return (
     <>
-      <ComponentBuilderPage id={1} />
-      <ArticleTeaserPage />
+      {/* <ComponentBuilderPage id={1} />
+      <ArticleTeaserPage /> */}
+      Test
     </>
   );
 };
+
+export async function getStaticProps({ preview = null }) {
+  const allExercises = (await getExercises(preview)) || [];
+  console.log(allExercises);
+  return {
+    props: { allExercises, preview },
+  };
+}
 
 export default Homepage;
