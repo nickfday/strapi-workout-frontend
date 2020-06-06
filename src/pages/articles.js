82801495a94df312500a2a0c8705690061e67a1e
Nicholas Finley-Day <nickfday@gmail.com>
2020-06-06 18:03:33 +0100

    add artiicles page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import ComponentBuilderPage from 'components/ComponentBuilder/page';
+// import React from 'react';
+// import ComponentBuilderPage from 'components/ComponentBuilder/page';
 
-const Page = () => {
-  return <ComponentBuilderPage id={4} />;
+// const Page = () => {
+//   return <ComponentBuilderPage id={4} />;
+// };
+
+// export default Page;
+
+// import React from 'react';
+// import ComponentBuilderPage from 'components/ComponentBuilder/page';
+
+// const Page = () => {
+//   return <ComponentBuilderPage id={3} />;
+// };
+
+// export default Page;
+
+import React from 'react';
+import ComponentBuilder from 'components/ComponentBuilder';
+import { ArticleList } from 'components/Article/list';
+import { getComponentBuilder, getArticles } from '../../lib/api';
+
+const Page = (articlesList) => {
+  console.log(articlesList);
+  return (
+    <>
+      <ArticleList articles={articlesList.articlesList} />
+    </>
+  );
 };
+
+export async function getStaticProps() {
+  const articlesList = (await getArticles()) || [];
+  console.log(articlesList);
+  return {
+    props: { articlesList },
+  };
+}
 
 export default Page;
