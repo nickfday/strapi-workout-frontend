827edc0c39bad226125f00a105f45b275332a4d0
Nicholas Finley-Day <nickfday@gmail.com>
2020-06-06 21:15:21 +0100

    add exercise deetail

--- src/pages/exercise/[id].js
+++ src/pages/exercise/[id].js
@@ -1,13 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import ExerciseSinglePage from 'components/Exercise/ExerciseSingle/page';
+// import React, { useEffect, useState } from 'react';
+// import ExerciseSinglePage from 'components/Exercise/ExerciseSingle/page';
 
-const Page = (props) => {
-  const [slug, setCount] = useState('');
-  useEffect(() => {
-    setCount(window.location.pathname.split('/').pop());
-  }, []);
+// const Page = (props) => {
+//   const [slug, setCount] = useState('');
+//   useEffect(() => {
+//     setCount(window.location.pathname.split('/').pop());
+//   }, []);
 
-  return <ExerciseSinglePage slug={slug} />;
+//   return <ExerciseSinglePage slug={slug} />;
+// };
+
+// export default Page;
+
+import React from 'react';
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+
+import { getExerciseDetail } from '../../lib/api';
+
+const Page = (exerciseDetail) => {
+  const router = useRouter();
+  // if (!router.isFallback && !post?.slug) {
+  //   return <ErrorPage statusCode={404} />;
+  // }
+  console.log(exerciseDetail);
+  return (
+    <>
+      Exercise Detail
+      {/* <ComponentBuilder {...componentBuilder.componentBuilder} /> */}
+    </>
+  );
 };
+
+export async function getStaticProps({ params }) {
+  const exerciseDetail = (await getExerciseDetail(params.slug)) || [];
+  console.log(exerciseDetail);
+  return {
+    props: { exerciseDetail, slug },
+  };
+}
 
 export default Page;
