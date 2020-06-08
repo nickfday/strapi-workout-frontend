f38a14cd94a64ca749566f35167cfb0f7792c143
Nicholas Finley-Day <nickfday@gmail.com>
2020-06-08 18:58:18 +0100

    remove page

--- src/components/ComponentBuilder/index.js
+++ src/components/ComponentBuilder/index.js
@@ -4,21 +4,11 @@ import dynamic from 'next/dynamic';
 const CtaBanner = dynamic(() => import('components/CtaBanner'));
 const CardGroup = dynamic(() => import('components/CardGroup'));
 const HeroBanner = dynamic(() => import('components/HeroBanner'));
-const FetchExercise = dynamic(() => import('components/Exercise/page'));
-const Article = dynamic(() =>
-  import('components/Article/page').then((item) => item.ArticleGroupPage),
-);
-const Routine = dynamic(() =>
-  import('components/Routine/page').then((item) => item.RoutineGroupPage),
-);
 
 const reactComponents = {
   ComponentGlobalCard: CardGroup,
   ComponentGlobalCtaBanner: CtaBanner,
   ComponentGlobalPageBanner: HeroBanner,
-  ComponentGlobalExercise: FetchExercise,
-  ComponentGlobalArticle: Article,
-  ComponentGlobalRoutine: Routine,
 };
 
 const ComponentBuilder = ({ components }) => {
