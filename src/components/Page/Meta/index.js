06ab347450e39435b73de09fb69736975e3fc6ac
Nicholas Finley-Day <nickfday@gmail.com>
2020-06-16 19:12:17 +0100

    add analytics

--- src/components/Page/Meta/index.js
+++ src/components/Page/Meta/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import { GA_TRACKING_ID } from '../../../../lib/gtag';
+
 const Meta = () => {
   return (
     <Head>
@@ -15,7 +17,25 @@ const Meta = () => {
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
       />
 
-      <title>Workout</title>
+      <title>Workout Mentor</title>
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </Head>
   );
 };
