80aa41c413dde22a6f2f4a5e2c1fdd94743db960
Nicholas Finley-Day <nickfday@gmail.com>
2020-06-30 02:15:50 +0100

    replace theme with ts

--- src/theme.ts
+++ src/theme.ts
@@ -3,12 +3,14 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export default createMuiTheme({
   palette: { primary: { main: '#373A3C' } },
   breakpoints: {
-    xs: 0,
-    sm: 600,
-    md: 960,
-    lg: 1280,
-    xl: 1920,
-    mobile: 320,
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+      // mobile: 320,
+    },
   },
   typography: {
     fontSize: 16,
